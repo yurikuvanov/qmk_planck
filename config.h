7f5078ae992e3a967354157e6f5c31d932e75e8f
yurikuvanov <lopakhin@gmail.com>
2019-06-14 09:58:50 +0900

    CtrlとWindows入れ替え

--- config.h
+++ config.h
@@ -37,4 +37,4 @@
 // Most tactile encoders have detents every 4 stages
 #define ENCODER_RESOLUTION 4
 
-#define TAPPING_TERM 135
+#define TAPPING_TERM 150
--- keymap.c
+++ keymap.c
@@ -51,14 +51,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  | , <  | . >  | / ?  |SHIFT |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Esc  | Ctrl | Alt  | GUI  |Lower | Space|Enter |Raise | Left | Down |  Up  |Right |
+ * | Esc  |  GUI | Alt  | Ctrl |Lower | Space|Enter |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_JIS] = LAYOUT_planck_grid(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    WN_SCLN, WN_MINS,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT ,
-    KC_ESC,     KC_LCTL, KC_LALT, KC_LGUI, LT(LOWER,JP_MHEN),KC_SPC,KC_ENT,LT(RAISE,JP_HENK),KC_LEFT, KC_DOWN, KC_UP,KC_RGHT
+    KC_ESC,  KC_LGUI, KC_LALT, KC_LCTR, LT(LOWER,JP_MHEN),KC_SPC,KC_ENT,LT(RAISE,JP_HENK),KC_LEFT, KC_DOWN, KC_UP,KC_RGHT
 ),
 
 /* Lower
